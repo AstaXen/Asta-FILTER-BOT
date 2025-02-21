0710f0fcbe5685679441dc62c41af28b70311acd
Asta <teamdesignduo@gmail.com>
2025-02-21 14:06:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://www.vecteezy.com/vector-art/2928032-smiling-cute-robot-chat-bot-support-service-concept-vector-cartoon-flat-illustration')).split()
+PICS = (environ.get('PICS', '')).split()
 
 
 # Admins & Users
